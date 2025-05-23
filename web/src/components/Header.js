ddebe41634203d18e2db46abb3b03f28959522a1
ahmetkemalbiliciler <kemalbiliciler35@gmail.com>
2025-05-24 01:10:55 +0300

    add Logout

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	AppBar,
 	Toolbar,
@@ -9,6 +9,8 @@ import {
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import storage from '../lib/storage';
 
 
 // Custom styled components using the color palette
@@ -25,48 +27,60 @@ const StyledButton = styled(Button)(({ theme }) => ({
 }));
 
 const Header = () => {
-	const navigate= useNavigate();
-	const handleLogin= () =>{
-		navigate('/login');
+	const user = useSelector(state => state.user);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const handleButton = async () => {
+		if (user) {
+			await storage.removeItem("token");
+
+			dispatch({
+				type: 'LOGOUT',
+			})
+		}
+		else navigate('/login');
 	}
-	const handleHome= () =>{
+	const handleHome = () => {
 		navigate('/');
 	}
+
+
 	return (
 		<StyledAppBar position="static">
-				<Toolbar disableGutters>
-					<Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, }}>
-						<Typography
-							variant="h6"
-							component="div"
-							sx={{
-								color: '#F1F2F6',
-								fontWeight: 'bold',
-								letterSpacing: '0.5px',
-								fontSize: '30px',
-								marginLeft: 2
-							}}
-						>
-							Train Ticket Distributor
-						</Typography>
-					</Box>
+			<Toolbar disableGutters>
+				<Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, }}>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{
+							color: '#F1F2F6',
+							fontWeight: 'bold',
+							letterSpacing: '0.5px',
+							fontSize: '30px',
+							marginLeft: 2
+						}}
+					>
+						Train Ticket Distributor
+					</Typography>
+				</Box>
 
-					<Box sx={{ display: 'flex', gap: 2, marginRight: 2 }}>
-						<StyledButton onClick={handleHome}>Home</StyledButton>
-						<StyledButton>About</StyledButton>
-						<StyledButton variant="contained"
-							sx={{
-								backgroundColor: '#FF8600',
-								'&:hover': {
-									backgroundColor: '#E67A00',
-								}
-							}}
-							onClick={handleLogin}
-						>
-							Admin Login
-						</StyledButton>
-					</Box>
-				</Toolbar>
+				<Box sx={{ display: 'flex', gap: 2, marginRight: 2 }}>
+					<StyledButton onClick={handleHome}>Home</StyledButton>
+					<StyledButton>About</StyledButton>
+					<StyledButton variant="contained"
+						sx={{
+							backgroundColor: '#FF8600',
+							'&:hover': {
+								backgroundColor: '#E67A00',
+							}
+						}}
+						onClick={handleButton}
+					>
+						{!user ? "Admin Login" : "Log Out"}
+					</StyledButton>
+				</Box>
+			</Toolbar>
 		</StyledAppBar>
 	);
 };
